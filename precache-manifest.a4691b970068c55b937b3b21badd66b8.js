920973c2adde0ff6359a30d881c3ad10183f9415
eglove <hello@ethang.email>
2020-09-06 13:53:51 -0500

    Updates

---
+++ precache-manifest.a4691b970068c55b937b3b21badd66b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec101d98bbda2d845c9c9e23b9e1e8d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "47e1933332c207c8649e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2b6bda2d98a97a8be1ad",
+    "url": "/robofriends/static/css/main.bc08ff9b.chunk.css"
+  },
+  {
+    "revision": "47e1933332c207c8649e",
+    "url": "/robofriends/static/js/2.58523ca3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.58523ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6bda2d98a97a8be1ad",
+    "url": "/robofriends/static/js/main.674c88a0.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
