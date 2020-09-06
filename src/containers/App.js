ec55f6fcc52403c8f29c722c61296d26daf2abca
eglove <hello@ethang.email>
2020-09-06 18:29:26 -0500

    Redux additions

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			robots: [],
-			searchField: '',
 		}
 	}
 
@@ -19,24 +32,20 @@ class App extends Component {
 			.then(users => this.setState({robots: users}));
 	}
 
-	onSearchChange = (event) => {
-		this.setState({
-			searchField: event.target.value,
-		});
-	}
-
 	render() {
+		const { robots } = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 
-		return !this.state.robots.length ?
+		return !robots.length ?
 			<h1 className='tc f1'>Loading..</h1>
 			:
 			(
 				<div className='tc'>
 					<h1 className='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
+					<SearchBox searchChange={onSearchChange}/>
 					<Scroll>
 						<ErrorBoundary>
 							<CardList robots={filteredRobots}/>
@@ -47,4 +56,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
