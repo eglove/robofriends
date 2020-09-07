93ae860f05762c1e877babff64dff219a4f3e3d0
eglove <hello@ethang.email>
2020-09-07 15:51:10 -0500

    code splitting

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {PureComponent} from 'react';
+
+class Header extends PureComponent {
+	render() {
+		return (
+			<>
+				<h1 className='f1'>RoboFriends</h1>
+			</>
+		);
+	}
+}
+
+export default Header;
